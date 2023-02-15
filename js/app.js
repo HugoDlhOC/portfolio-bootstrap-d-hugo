f94be3737476ffa259ffa8f78f1cf6182571f89c
HugoDlhOC <hugo.delahaye53@outlook.fr>
2023-02-15 23:29:15 +0100

    add files - correct responsive md projects section

---
+++ js/app.js
@@ -0,0 +1,5 @@
+//js tooltip
+let tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+let tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+})
